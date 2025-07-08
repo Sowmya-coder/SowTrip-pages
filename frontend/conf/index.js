2b4b0b6e0e9d8f365a813220cf7bfc82e644c59d
Sowmya godavarthi <sowmyagodavarthi513@gmail.com>
2025-07-08 21:40:03 +0530

    Final push

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,6 @@
 
-const config = { backendEndpoint: "http://127.0.0.1:8082" };
+// const config = { backendEndpoint: "http://127.0.0.1:8082" };
+const config = { backendEndpoint: "https://sowtripwebsite.onrender.com" };
+
 
 export default config;
